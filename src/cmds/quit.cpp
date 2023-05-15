5426f86c19fd4633d520d39dbbd3f9ab0bc89431
Clement Paluszek <cpalusze@z4r10p1.42lyon.fr>
2023-05-15 09:42:25 +0200

    add todos in quit cmd

--- src/cmds/quit.cpp
+++ src/cmds/quit.cpp
@@ -5,5 +5,8 @@ void quitCmd(Client *client, const Request &request, Server *server) {
 	(void) server;
 	(void) request;
 	(void) client;
+	// Todo: remove client from map
+	// Todo: leave all channels that the user is in
+	// Todo: close the client socket and remove from poll
 	std::cout << "QUIT not implemented" << std::endl;
 }
