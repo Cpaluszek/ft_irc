51023d2786cfa2db80df557c5f5cd289cf76421f
aurelien <aurelien.calandrino@outlook.fr>
2023-06-01 11:53:24 +0200

    parsing MODE done, exec in progress
    + add some setter/getter in channel mod

--- src/cmds/mods.cpp
+++ src/cmds/mods.cpp
@@ -65,58 +65,24 @@ std::vector<int> getFlags( const Request &request, int mode )
 		// Get mod in a Vector<int>, to call them more explicitly
 		switch ( arg[i] ) {
 			case 'i' :
-			{
-				if ( mode == USERMOD )
-				{
-					if ( typeOfFlag == ADD )
-						flags.push_back( I_ADD_INVISIBLE_USERMOD );
-					else
-						flags.push_back( I_RM_INVISIBLE_USERMOD );
-				}
-				else
-				{
-					if ( typeOfFlag == ADD )
-						flags.push_back( I_ADD_INVITEONLY_CHANNELMOD );
-					else
-						flags.push_back( I_RM_INVITEONLY_CHANNELMOD );
-				}
+				typeOfFlag == ADD ? flags.push_back( I_ADD_INVITEONLY_CHANNELMOD ) : flags.push_back( I_RM_INVITEONLY_CHANNELMOD );
 				break;
-			}
 			case 'o':
 			{
 				if ( mode == USERMOD )
-				{
-					if ( typeOfFlag == ADD )
-						flags.push_back( O_ADD_OP_USERMOD );
-					else
-						flags.push_back( O_RM_OP_USERMOD );
-				}
-				break;
-			}
-			case 'r':
-			{
-				if ( mode == USERMOD )
-				{
-					if ( typeOfFlag == ADD )
-						flags.push_back( R_ADD_REGISITERED_USERMOD );
-					else
-						flags.push_back( R_RM_REGISTERED_USERMOD );
-				}
-				break;
-			}
-			case 'e':
-			{
-				if ( typeOfFlag == ADD )
-					flags.push_back( E_ADD_BANEXCEPTIONLIST_CHANNELMOD );
+					typeOfFlag == ADD ? flags.push_back( O_ADD_OP_USERMOD ) : flags.push_back( O_RM_OP_USERMOD );
 				else
-					flags.push_back( E_ADD_BANEXCEPTIONLIST_CHANNELMOD );
+					typeOfFlag == ADD ? flags.push_back( O_ADD_OP_CHANNELMOD ) : flags.push_back( O_RM_OP_CHANNELMOD );
 				break;
 			}
-			case 'b':
-				if ( typeOfFlag == ADD )
-					flags.push_back( B_ADD_BANLIST_CHANNELMOD );
-				else
-					flags.push_back( B_ADD_BANLIST_CHANNELMOD );
+			case 't':
+				typeOfFlag == ADD ? flags.push_back( T_ADD_PROTECTEDTOPIC_CHANNELMOD ) : flags.push_back( T_RM_PROTECTEDTOPIC_CHANNELMOD );
+				break;
+			case 'k':
+				typeOfFlag == ADD ? flags.push_back( K_ADD_KEY_CHANNELMOD ) : flags.push_back( K_RM_KEY_CHANNELMOD );
+				break;
+			case 'l':
+				typeOfFlag == ADD ? flags.push_back( L_ADD_CLIENTLIMIT_CHANNELMOD ) : flags.push_back( L_RM_CLIENTLIMIT_CHANNELMOD );
 				break;
 			case ' ':
 				return flags;
@@ -127,21 +93,71 @@ std::vector<int> getFlags( const Request &request, int mode )
 	return flags;
 }//TODO: implement all mod
 
+
+static void executeModeCmd( Client *client, Server *server, const Request &request, std::vector<int> flags, Channel *channel)
+{
+	(void)client;
+	(void)server;
+	(void)request;
+	(void)flags;
+	(void)channel;
+	std::vector<int>::iterator itFlags = flags.begin();
+	std::vector<int>::iterator itFlagsEnd = flags.end();
+	for (; itFlags != itFlagsEnd ; ++itFlags) {
+		switch ( *itFlags ) {
+			case O_ADD_OP_CHANNELMOD:
+				break;
+			case O_RM_OP_CHANNELMOD:
+				break;
+			case O_ADD_OP_USERMOD:
+				break;
+			case O_RM_OP_USERMOD:
+				break;
+			case L_ADD_CLIENTLIMIT_CHANNELMOD:
+				break;
+			case L_RM_CLIENTLIMIT_CHANNELMOD :
+				break;
+			case I_ADD_INVITEONLY_CHANNELMOD:
+				break;
+			case I_RM_INVITEONLY_CHANNELMOD:
+				break;
+			case T_ADD_PROTECTEDTOPIC_CHANNELMOD:
+				break;
+			case T_RM_PROTECTEDTOPIC_CHANNELMOD:
+				break;
+			case K_ADD_KEY_CHANNELMOD:
+				break;
+			case K_RM_KEY_CHANNELMOD:
+				break;
+			default:
+				break;
+		}
+	}
+}
+
 void mode( Client *client, const Request &request, Server *server )
 {
 	(void) client;
 	(void) server;
 
+	Channel *channel;
 	if ( !request.requestModeIsValid( client, server ) )
 		return ;
+	if ( request.args.begin()->find('#', 0) != std::string::npos )
+		channel = server->getChannelByName( *request.args.begin() )->second;
+	else
+		channel = NULL;
 	std::vector<int>	flags;
 	std::vector<std::string>::const_iterator itArgs = request.args.begin();
 	if ( itArgs[0].find('#', 0) != std::string::npos )
 		flags = getFlags( request, CHANNELMOD );
 	else
 		flags = getFlags( request, USERMOD );
-	std::vector<int>::iterator itprint = flags.begin();
-	for (;itprint != flags.end() ; itprint++) {
-		std::cout << *itprint << std::endl;
-	}
+	executeModeCmd( client, server, request, flags, channel );
+//	std::vector<int>::iterator itprint = flags.begin();
+//	for (;itprint != flags.end() ; itprint++) {
+//		std::cout << *itprint << std::endl;
+//	}
+
+
 }
