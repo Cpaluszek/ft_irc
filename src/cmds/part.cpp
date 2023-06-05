1ba728a9efd1b1c0f1bc913efce82ad70d23a285
Clement Paluszek <cpalusze@z3r11p5.42lyon.fr>
2023-06-05 13:21:45 +0200

    PART multiple channels OK

--- src/cmds/part.cpp
+++ src/cmds/part.cpp
@@ -9,15 +9,19 @@ void	partCmd(Client *client, const Request &request, Server *server) {
 		Server::sendToClient(client->socketFd, ERR_NEEDMOREPARAMS(client->nickName, request.command));
 		return ;
 	}
-	std::string targetChannel = request.args[0];
+	Server::vecStr targetChannels = Utils::split(request.args[0], ",", false);
 	std::string reason = request.args.size() > 1 ? request.args[1] : "No reason";
-	Channel *channel = server->getChannelByName(targetChannel);
-	if (channel == NULL)
-		Server::sendToClient(client->socketFd, ERR_NOSUCHCHANNEL(client->nickName, targetChannel));
-	else if (!client->isOnChannel(targetChannel))
-		Server::sendToClient(client->socketFd, ERR_NOTONCHANNEL(client->nickName, targetChannel));
-	else
-		leaveChannel(client, channel, reason);
+	for (Server::vecStrIt it = targetChannels.begin(); it != targetChannels.end(); it++) {
+		std::string targetChannel = *it;
+		Channel *channel = server->getChannelByName(targetChannel);
+		if (channel == NULL)
+			Server::sendToClient(client->socketFd, ERR_NOSUCHCHANNEL(client->nickName, targetChannel));
+		else if (!client->isOnChannel(targetChannel))
+			Server::sendToClient(client->socketFd, ERR_NOTONCHANNEL(client->nickName, targetChannel));
+		else
+			leaveChannel(client, channel, reason);
+	}
+
 }
 
 void leaveChannel(Client *client, Channel *channel, const std::string& reason) {
