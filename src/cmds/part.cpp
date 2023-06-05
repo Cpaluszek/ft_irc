0049bc2e9b2c37d210cae72bd9540863edb874fb
Clement Paluszek <cpalusze@z3r11p5.42lyon.fr>
2023-06-05 11:55:21 +0200

    PART reason

--- src/cmds/part.cpp
+++ src/cmds/part.cpp
@@ -1,35 +1,31 @@
 
 #include "commands.hpp"
 
-void	quitChannel(Client *client, Channel *channel);
+void leaveChannel(Client *client, Channel *channel, const std::string& reason);
 
+// Todo: check for multi channel part command
 void	partCmd(Client *client, const Request &request, Server *server) {
 	if (request.args.empty()){
 		Server::sendToClient(client->socketFd, ERR_NEEDMOREPARAMS(client->nickName, request.command));
 		return ;
 	}
-	std::vector<std::string>::const_iterator it = request.args.begin();
-	for (; it != request.args.end(); it++) 	{
-		Channel *channel = server->getChannelByName(*it);
-		if (channel == NULL)
-			Server::sendToClient(client->socketFd, ERR_NOSUCHCHANNEL(client->nickName, *it));
-		else if (!client->isOnChannel(*it))
-			Server::sendToClient(client->socketFd, ERR_NOTONCHANNEL(client->nickName, *it));
-		else {
-			quitChannel(client, channel);
-			break ;
-		}
-	}
-
+	std::string targetChannel = request.args[0];
+	std::string reason = request.args.size() > 1 ? request.args[1] : "No reason";
+	Channel *channel = server->getChannelByName(targetChannel);
+	if (channel == NULL)
+		Server::sendToClient(client->socketFd, ERR_NOSUCHCHANNEL(client->nickName, targetChannel));
+	else if (!client->isOnChannel(targetChannel))
+		Server::sendToClient(client->socketFd, ERR_NOTONCHANNEL(client->nickName, targetChannel));
+	else
+		leaveChannel(client, channel, reason);
 }
 
-// Todo: leave all channels is not always correct reason
-void	quitChannel(Client *client, Channel *channel) {
+void leaveChannel(Client *client, Channel *channel, const std::string& reason) {
 	client->eraseChannel(channel->getName());
 	size_t channelUserCount = channel->getClientCount();
 	channel->eraseClient(client->nickName);
 	if (channelUserCount > 1) {
 		channel->sendToAllClients(
-				RPL_CMD(client->nickName, client->userName, "PART", (channel->getName() + " has leaveAllChannels")));
+				RPL_CMD(client->nickName, client->userName, "PART", (channel->getName() + " " + reason)));
 	}
 }
