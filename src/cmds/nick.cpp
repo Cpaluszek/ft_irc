f7473976a5398d3ce8485947418ad7118c4fab03
Clement Paluszek <cpalusze@z4r11p1.42lyon.fr>
2023-06-12 10:11:13 +0200

    Reduce nesting nick.cpp

--- src/cmds/nick.cpp
+++ src/cmds/nick.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   nick.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aurel <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 13:44:53 by cpalusze          #+#    #+#             */
-/*   Updated: 2023/06/11 18:34:42 by aurel            ###   ########.fr       */
+/*   Updated: 2023/06/12 10:09:06 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 bool containsInvalidChars(std::string nick) {
 	return (nick.find_first_of(" ,*?!@.") != std::string::npos ||
-	 nick[0] == '$' || nick[0] == ':' || nick[0] == '#' || nick[0] == '&');
+			nick[0] == '$' || nick[0] == ':' || nick[0] == '#' || nick[0] == '&');
 }
 
 // [IRC Client Protocol Specification](https://modern.ircdocs.horse/#nick-message)
@@ -24,42 +24,41 @@ void nickCmd(Client *client, const Request &request, Server *server) {
 		return ;
 	}
 	// No nickname given
-	std::string nick;
-	if ( !request.args.empty() && request.args[0].length() != 0 )
-		nick = request.args[0];
-	else {
+	if ( request.args.empty() || request.args[0].length() == 0 ) {
 		Server::sendToClient(client->socketFd, ERR_NONICKNAMEGIVEN(std::string("Client")));
+		return ;
 	}
+	std::string nick = request.args[0];
 	// Invalid characters
 	if (containsInvalidChars(nick)) {
 		Server::sendToClient(client->socketFd, ERR_ERRONEUSNICKNAME(nick));
+		return ;
 	}
 	// Nickname already used
-	else if (server->isNickAlreadyUsed(*client, nick)) {
+	if (server->isNickAlreadyUsed(*client, nick)) {
 		Server::sendToClient(client->socketFd, ERR_NICKNAMEINUSE(nick));
+		return ;
 	}
 	// Success
-	else {
-		client->updateNickname(nick);
-		if (!client->hasMode('r') && client->userName.length() != 0) {  // First nickname input
-			client->addMode('r');
-			server->sendWelcome(client);
-		}
-		else { // Update nickname
-			Server::sendToClient(client->socketFd, RPL_NICK(client->previousNickname, nick, client->userName));
-			Client::channelMap channels = client->getChannels();
-			Client::channelMapIt it;
-			for (it = channels.begin(); it != channels.end(); it++) {
-				it->second->updateClient(client->previousNickname, client->nickName);
+	client->updateNickname(nick);
+	if (!client->hasMode('r') && client->userName.length() != 0) {  // First nickname input
+		client->addMode('r');
+		server->sendWelcome(client);
+	}
+	else { // Update nickname
+		Server::sendToClient(client->socketFd, RPL_NICK(client->previousNickname, nick, client->userName));
+		Client::channelMap channels = client->getChannels();
+		Client::channelMapIt it;
+		for (it = channels.begin(); it != channels.end(); it++) {
+			it->second->updateClient(client->previousNickname, client->nickName);
 
-				// Alert all users in the channel that the nickname has changed
-				Channel::mapClients channelClients = it->second->getClients();
-				Channel::mapClientsIt channelClientIt;
-				for (channelClientIt = channelClients.begin(); channelClientIt != channelClients.end(); channelClientIt++) {
-					Client *current = channelClientIt->second.client;
-					if (current->nickName != client->nickName) {
-						Server::sendToClient(current->socketFd, RPL_NICK(client->previousNickname, client->nickName, client->userName));
-					}
+			// Alert all users in the channel that the nickname has changed
+			Channel::mapClients channelClients = it->second->getClients();
+			Channel::mapClientsIt channelClientIt;
+			for (channelClientIt = channelClients.begin(); channelClientIt != channelClients.end(); channelClientIt++) {
+				Client *current = channelClientIt->second.client;
+				if (current->nickName != client->nickName) {
+					Server::sendToClient(current->socketFd, RPL_NICK(client->previousNickname, client->nickName, client->userName));
 				}
 			}
 		}
